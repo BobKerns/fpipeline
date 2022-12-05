0e862e79fb7a54b7fcbecc38f838c988ec96009e
Bob Kerns <1154903+BobKerns@users.noreply.github.com>
2022-12-05 13:26:07 +0000

    Release 0.0.5

--- src/fpipeline/__init__.py
+++ src/fpipeline/__init__.py
@@ -3,3 +3,5 @@
 from .fpipeline import pipeline, variables, stepfn, conditionfn, \
     if_, not_, and_, or_, Variable, Attribute, VariableContext, store, eval_, \
     list_, dict_, tuple_
+
+from ._version import __version__
